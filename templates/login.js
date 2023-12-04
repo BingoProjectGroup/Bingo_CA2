5485115312e21bde8a621c4802161459fcfc0eb2
AnaghaAnil1 <86355621+AnaghaAnil1@users.noreply.github.com>
2023-12-04 16:44:34 +0000

    login js update

---
+++ templates/login.js
@@ -0,0 +1,26 @@
+function validateEmail() {
+    var emailInput = document.getElementsByName('email')[0];
+    var emailError = document.getElementById('emailError');
+  
+    
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var email = emailInput.value.trim();
+  
+    if (!emailRegex.test(email)) {
+      emailError.textContent = 'Invalid email format';
+      return false;
+    } else {
+      emailError.textContent = '';
+      return true;
+    }
+  }
+  // Prevent form submission if email is invalid
+  document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+      if (!validateEmail()) {
+        event.preventDefault(); 
+      }
+    });
+  });
+  
