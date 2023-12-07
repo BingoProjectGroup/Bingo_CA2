0bd685edbd5ecfe358ab6a937ce63517cf2c5cf1
boncybabu1997 <152879488+boncybabu1997@users.noreply.github.com>
2023-12-07 20:01:41 +0000

    Create admindash.js
    added validation

---
+++ static/admindash.js
@@ -0,0 +1,14 @@
+function addBook() {
+    // Logic for adding a book
+    alert('Adding a book');
+}
+
+function removeBook() {
+    // Logic for removing a book
+    alert('Removing a book');
+}
+
+function updateBook() {
+    // Logic for updating a book
+    alert('Updating a book');
+}
