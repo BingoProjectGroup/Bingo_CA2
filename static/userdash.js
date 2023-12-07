189ca104e1812ecbb15861641e9f1d07ce8ee7a1
boncybabu1997 <152879488+boncybabu1997@users.noreply.github.com>
2023-12-07 20:07:14 +0000

    Create userdash.js
    added validation

---
+++ static/userdash.js
@@ -0,0 +1,10 @@
+function seeAllBooks() {
+    // Logic for displaying all books
+    alert('Displaying all books');
+}
+
+function searchBooks() {
+    // Logic for searching books
+    var searchInput = document.getElementById('searchInput').value;
+    alert('Searching for: ' + searchInput);
+}
