dead9ef21ce16764ac6057ed11648675fff2ed12
chippu <35302582+chippu@users.noreply.github.com>
2023-12-09 22:26:52 +0000

    Update urls.py
    added delete functionality

--- brmapp/urls.py
+++ brmapp/urls.py
@@ -1,15 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path,include,re_path
-from .views import helloView,addBookView,addBook,editBookView,editBook
+from .views import helloView,addBookView,addBook,editBookView,editBook,deleteBookView
 
 urlpatterns = [
     
     path("",helloView),
     path("add-book/",addBookView),
-    path("add-book/add",addBook),
+    path("add-book/addbookdata",addBook),
     path("edit-book/",editBookView),
-    path("edit-book/edit",editBook)
+    path("edit-book/edit",editBook),
+    path("delete-book/",deleteBookView),
     
     
     
