fe41446d0e553561c3e6aff77db5292dbfdb35b0
SheejaLawrance <66172055+SheejaLawrance@users.noreply.github.com>
2023-12-11 21:36:55 +0000

    submit added in register form

--- brmapp/views.py
+++ brmapp/views.py
@@ -9,6 +9,12 @@ from.models import Book
 
 def registerPage(request):
     form=UserCreationForm()
+
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     context={'form':form}
     return render(request,'register.html', context)
 
