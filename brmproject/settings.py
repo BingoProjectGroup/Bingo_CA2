4e03591f25d950060d8bf3125588158c302c39d7
SheejaLawrance <66172055+SheejaLawrance@users.noreply.github.com>
2023-12-11 12:29:44 +0000

    allowed_host='*' so that every one could work on it

--- brmproject/settings.py
+++ brmproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$&$&f%g6j1bpq7-)6r8j+j3kkwpkr3ls2$ugk58zk8)&#ej@s_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
