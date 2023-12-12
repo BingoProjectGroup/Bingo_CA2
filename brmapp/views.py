acf45f95a222590b4467139fc5bb699d49dddf2c
boncybabu1997 <152879488+boncybabu1997@users.noreply.github.com>
2023-12-12 06:22:02 +0000

    Update views.py
    added function of login

--- brmapp/views.py
+++ brmapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
@@ -24,6 +26,17 @@ def registerPage(request):
     return render(request,'register.html', context)
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password
+
+        if user is not None:
+            login(request, username)
+            redirect('home')
+                            
+
     context={}
     return render(request,'login.html', context)
 
