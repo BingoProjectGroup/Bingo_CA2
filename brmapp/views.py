f0e7bd47dc9695368f5235ac191084fed7c66d32
SheejaLawrance <66172055+SheejaLawrance@users.noreply.github.com>
2023-12-12 02:45:06 +0000

    register.html after registeration redirect to login path updated

--- brmapp/views.py
+++ brmapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 from.models import Book
@@ -15,7 +16,9 @@ def registerPage(request):
         form=CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            user=form.cleaned_data.get('username')
+            messages.success(request, 'Hi'+ user + '/n Registration is Succesful!')
+            return redirect('/login')
 
     context={'form':form}
     return render(request,'register.html', context)
