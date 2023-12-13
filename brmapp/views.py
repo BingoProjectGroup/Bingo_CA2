ffe55024614f38932b481c751d03a02bead2da45
SheejaLawrance <66172055+SheejaLawrance@users.noreply.github.com>
2023-12-13 01:41:29 +0000

    login with username and password

--- brmapp/views.py
+++ brmapp/views.py
@@ -18,7 +18,7 @@ def registerPage(request):
         if form.is_valid():
             form.save()
             user=form.cleaned_data.get('username')
-            messages.success(request, 'Hi'+ user + '/n Registration is Succesful!')
+            messages.success(request, 'Hi'+ user + ' Registration is Succesful!')
             return redirect('/login')
 
     context={'form':form}
@@ -26,10 +26,10 @@ def registerPage(request):
 
 def loginPage(request):
     if request.method=='POST':
-        email=request.POST.get('email')
+        username=request.POST.get('username')
         password=request.POST.get('password')
 
-        user=authenticate(request, email=email, password=password)
+        user=authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
